046e0a0d064ac2c78fb06d44360ff5eff0370477
sukhjinder-sdei <sukhjinder.singh@smartdatainc.net>
2019-07-20 02:44:28 +0530

    Comments added

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -1,26 +1,29 @@
 import pytest
-import json
 from datetime import datetime
 
 from todo_api_automation.rest_util import Rest_Util
+
+pytestmark = pytest.mark.sanity
 
 rest = Rest_Util()
 
 
 class Test_Todo:
-    task_id=''
-    summary=''
+    """ This is a test class to perform all sanity test for app's rest api """
+
+    task_id = ''  # Hold task id to use in different tests
+    summary = ''  # Hold summary to use in different tests
 
     def test_add_new_task(self):
         print ('Test: Verify new task should be created')
         now = datetime.now()
         task_summary = 'This is automation ' + str(now)
-        Test_Todo.summary =task_summary
+        Test_Todo.summary = task_summary
         res = rest.add_task(task_summary)
         assert res.status_code == 200
 
         Test_Todo.task_id = res.json().get('task_id')
-        print ('Task created with id '+ Test_Todo.task_id)
+        print ('Task created with id ' + Test_Todo.task_id)
 
         print ('Verify new task added with summary: ' + task_summary)
         res = rest.get_task(Test_Todo.task_id)
@@ -28,7 +31,6 @@ class Test_Todo:
         dict = res.json()
 
         assert dict.get('task') == task_summary
-
 
     def test_get_tasks(self):
         print('Test: Get all the tasks')
@@ -38,7 +40,6 @@ class Test_Todo:
 
         print ('Verify Content-Type should be "application/json"')
         assert res.headers.get('Content-Type') == 'application/json'
-
 
     def test_get_task_with_id(self):
         print ('Test: Verify task should be retrieved by using task id')
@@ -52,8 +53,6 @@ class Test_Todo:
         dict = res.json()
 
         assert dict.get('task') == Test_Todo.summary
-
-
 
     def test_update_task(self):
         print ('Test: Verify existing task can be updated')
@@ -74,10 +73,9 @@ class Test_Todo:
 
         assert dict.get('task') == task_summary
 
-
     def test_complete_task(self):
         print ('Test: Verify existing task can not be updated whe using method post')
-        res=rest.complete_task(Test_Todo.task_id)
+        res = rest.complete_task(Test_Todo.task_id)
         print ('Contents from post to update existing tasks using id: ' + Test_Todo.task_id)
         print ('Contents: ' + res.content)
         assert res.status_code == 405
@@ -88,11 +86,10 @@ class Test_Todo:
         assert res.status_code == 200
 
         print ('Verify task deleted ')
-        res =rest.get_task(Test_Todo.task_id)
+        res = rest.get_task(Test_Todo.task_id)
         dict = {}
         dict = res.json()
         assert len(dict.keys()) == 0
-
 
     if __name__ == '__main__':
         pytest.main()
--- todo_api_automation/rest_util.py
+++ todo_api_automation/rest_util.py
@@ -1,35 +1,83 @@
 import requests
-import json
 
 
 def _url(end_point):
+    """ The function to create uri with end point
+
+    Parameters:
+        end_point
+
+     Returns:
+            Rest resource uri
+    """
+
     return 'http://localhost:5000' + end_point
 
 
 class Rest_Util:
+    """ This class to perform all the operation with app's rest api """
+
     def get_tasks(self):
+        """
+        The function to get all tasks from apps's rest server.
+
+        Parameters:
+
+        Returns:
+            Response: This function will return rest response
+        """
+
         uri = _url('/tasks')
         print('Get all todo tasks: ' + uri)
-        return requests.get(_url('/tasks'))
+        return requests.get(uri)
+
 
     def get_task(self, task_id):
+        """
+                The function to get single task from apps's rest server.
+
+                Parameters:
+                    task_id
+
+                Returns:
+                    Response: This function will return rest response
+                """
         uri = _url('/tasks/{0}').format(task_id)
         print('Get single todo tasks: ' + uri)
         return requests.get(uri)
 
+
     def add_task(self, summary):
+        """
+                        The function to add new task in apps's rest server.
+
+                        Parameters:
+                            summary
+
+                        Returns:
+                            Response: This function will return rest response
+                        """
         uri = _url('/tasks')
         print('Add new tasks with summary: ' + summary)
 
-        return requests.post(_url('/tasks'), json={
+        return requests.post(uri, json={
             'task': summary,
             'completed': False
         })
 
-    def task_done(task_id):
-        pass
+
 
     def update_task(self, task_id, summary):
+        """
+                                The function to update existing task in apps's rest server.
+
+                                Parameters:
+                                    task_id
+                                    summary
+
+                                Returns:
+                                    Response: This function will return rest response
+                                """
         uri = _url('/tasks/{0}'.format(task_id))
         print('Update tasks with summary: ' + summary)
 
@@ -39,6 +87,16 @@ class Rest_Util:
         })
 
     def complete_task(self, task_id):
+        """
+                                The function to mark as complete existing task in apps's rest server.
+
+                                Parameters:
+                                    task_id
+
+                                Returns:
+                                    Response: This function will return rest response
+                                """
+
         uri = _url('/tasks/{0}'.format(task_id))
         print('Complete tasks with id: ' + task_id)
 
@@ -47,6 +105,16 @@ class Rest_Util:
         })
 
     def delete_task(self, task_id):
+        """
+                                The function to delete a single task in apps's rest server.
+
+                                Parameters:
+                                    task_id
+
+                                Returns:
+                                    Response: This function will return rest response
+                                """
+
         uri = _url('/tasks/{0}'.format(task_id))
         print('Delete tasks with id: ' + task_id)
         return requests.delete(uri)
